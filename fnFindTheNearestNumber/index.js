a9fb24a9ff044cdc6dacf289ab730d01ce582a21
salvimateus <salvi.mateus@gmail.com>
2022-01-18 12:22:10 -0300

    nearest number with miner of max distance

--- fnFindTheNearestNumber/index.js
+++ fnFindTheNearestNumber/index.js
@@ -28,10 +28,29 @@ const findTheNearestNumber = (target, arr) => {
   }
 }
 
-const randomArr = Array.from({ length: 1000 }, () =>
-  Math.floor(Math.random() * 1_000_000)
-)
+const randomArr = (size) =>
+  Array.from({ length: size }, () => Math.floor(Math.random() * 1_000_000))
 
-console.log(findTheNearestNumber(1992, randomArr))
-console.log(findTheNearestNumber(1, randomArr))
-console.log(findTheNearestNumber(500_000, randomArr))
+console.log(findTheNearestNumber(1992, randomArr(100)))
+console.log(findTheNearestNumber(1, randomArr(200)))
+console.log(findTheNearestNumber(500_000, randomArr(300)))
+
+const miner = (target, maxDistance) => {
+  let count = 1
+  let stopped = false
+  let result
+
+  while (!stopped) {
+    result = findTheNearestNumber(target, randomArr(50))
+
+    if (Math.abs(result.distance.replace(/\D/g, '')) <= Math.abs(maxDistance)) {
+      stopped = true
+    }
+    count++
+  }
+
+  return { count, result }
+}
+
+console.log(miner(1992, 50))
+console.log(miner(999_000, 1))
